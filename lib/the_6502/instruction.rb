364835cd2cea3d9a15a51943cc5a69f92d4a22e2
Darren Cauthon <darren@cauthon.com>
2016-06-19 19:58:53 +0000

    DRY up the logic to get the operand.

--- lib/the_6502/instruction.rb
+++ lib/the_6502/instruction.rb
@@ -9,5 +9,13 @@ module The6502
     def size_of instruction
       2
     end
+
+    def operands_from instruction
+      items = instruction.split(' ')
+      items.shift
+      items
+        .map { |x| { original: x, resolved: x.split('$')[1].to_i(16) } }
+        .map { |x| x[:original].include?('#') ? x[:resolved] : processor.memory_at(x[:resolved]) }
+    end
   end
 end
--- lib/the_6502/instructions/ldy.rb
+++ lib/the_6502/instructions/ldy.rb
@@ -3,9 +3,7 @@ module The6502
   class LDY < The6502::Instruction
 
     def execute instruction
-      value = instruction.split('$')[1].to_i(16)
-      value = processor.memory_at(value) unless instruction.include?('#')
-      processor.y = value
+      processor.y = operands_from(instruction)[0]
     end
 
   end
