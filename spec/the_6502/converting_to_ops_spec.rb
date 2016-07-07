c68a978a712dbb1290eaacd4b26ebda8dbae8cc4
Darren Cauthon <darren@cauthon.com>
2016-07-07 02:56:12 +0000

    Fix the spacing.

--- spec/the_6502/converting_to_ops_spec.rb
+++ spec/the_6502/converting_to_ops_spec.rb
@@ -4,14 +4,14 @@ describe "new memory" do
 
   let(:processor) { The6502::Processor.new }
 
-    it "should be set to true when the math on a is greater than a byte" do
-      processor.execute(
+  it "should be set to true when the math on a is greater than a byte" do
+    processor.execute(
 <<EOF
 LDA \#$01
 EOF
-      )
-      processor.new_memory[0].must_equal 0xa9
-      processor.new_memory[1].must_equal 0x01
-    end
+    )
+    processor.new_memory[0].must_equal 0xa9
+    processor.new_memory[1].must_equal 0x01
+  end
 
 end
